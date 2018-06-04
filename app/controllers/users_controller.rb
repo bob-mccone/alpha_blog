51deadda3d58d4966d5f8f7c51b803e3d0abf6a1
bob mccone <brudderbob07@gmail.com>
2018-06-04 10:50:29 -0400

    Create show action for user and add gravatar image

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -34,6 +34,10 @@ class UsersController < ApplicationController
     end
   end
   
+  def show
+    @user = User.find(params[:id])
+  end
+  
   private
   #User parameters
   def user_params
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  #Gravatar_for method, including the size of the picture
+  def gravatar_for(user, options = { size: 80 })
+    #Gravatar image is MD5
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    size = options[:size]
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+    image_tag(gravatar_url, alt: user.username, class: "img-circle")
+  end
 end
