0f1b33f446b524e994fa57dd7ec4147413fae94d
bob mccone <brudderbob07@gmail.com>
2018-07-16 13:40:10 -0400

    Fixed list categories test bug

--- test/integration/list_categories_test.rb
+++ test/integration/list_categories_test.rb
@@ -1,9 +1,9 @@
 require 'test_helper'
 #Integration categories testing
-class CreateCategoriesTest < ActionDispatch::IntegrationTest
+class ListCategoriesTest < ActionDispatch::IntegrationTest
   #setup method
   def setup
-    @category = Category.create(name: "books")
+    @category = Category.create(name: "sports")
     @category2 = Category.create(name: "programming")
   end
   
