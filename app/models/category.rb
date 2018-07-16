f61e501c6e4ce4c653aafba168e558a925aea916
bob mccone <brudderbob07@gmail.com>
2018-07-16 17:36:45 -0400

    Add article and category association

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,8 @@
 #Category class, need this to pass the test
 class Category < ActiveRecord::Base
+  has_many :article_categories
+  #Many article via article_categories table
+  has_many :articles, through: :article_categories
   #Validation for name must exist and be between 3 and 25 characters
   validates :name, presence: true, length: { minimum: 3, maximum: 25 }
   #validation for name uniqueness
